d80f1129f59dfe4610d79d1f4bf7f4d5b8c05ef3
swhp <sukma.wardana@mail.com>
2016-03-26 04:13:23 +0700

    create dummy Authentication

--- app/app-services/AuthService.js
+++ app/app-services/AuthService.js
@@ -9,22 +9,53 @@
  */
 
 angular.module('nanodesuApp')
-    .factory('AuthService', function($resource){
-        return{
-            login: function(username, password){
-                if(username == 'admin' && password == 'admin'){
-                    return "Success";
-                }else{
-                    return "Failed";    
-                }
-            },
-            isLogin: function(param){
-                var isLogin = param;
-                return isLogin;
-            },
-            logout: function(){
-            },
-            register: function(){
+    .factory('AuthService', function($http,$cookies){
+        var auth = {}
+
+        auth.login = login;
+        auth.logout = logout;
+        auth.isLogin = isLogin;
+        //auth.register = register;
+
+        /**
+        //private functions
+        function handleSuccess(res){
+            return res.data;
+        }
+
+        function handleError(error){
+            return function(){
+                return { success: false, message: error };
             }
         }
+        */
+
+        function login(username, password){
+            //return $http.post('/login',data).then(handleSuccess, handleError('login failed'));
+            var status;
+            if(username == "admin" && password == "admin"){
+                alert('success');
+                status = true;
+            }else{                
+                alert('failed');
+                status = false;
+            }
+            return status;
+        }
+
+        function isLogin(){
+            var login;
+            if($cookies.get('auth')){
+                login = true;
+            }else{
+                login = false;
+            }
+            return login;
+        }
+
+        function logout(){
+            $cookies.remove('auth');
+        }
+
+        return auth;
     });

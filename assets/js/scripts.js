0c108839a03dc3a97f6c39b52a586d13fc3c7d31
DerekRS <sankyured@hotmail.com>
2016-07-10 20:27:08 -0700

    Add back scripts.js ($nd utility assert/logging module)

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+﻿// $nd object for app-specific debug utilities
+var $nd = (function () {
+    var NDebug = {};
+    NDebug.assert = function (fTest, message) {
+        if (fTest) return;
+        if (window.console && console.assert) {
+            window.console.assert(fTest, message);
+        }
+        debugger; // TODO: do we need a switch to enable this?
+    }
+
+    NDebug.warn = function (s) {
+        if (window.console && console.error) {
+            window.console.error(s);
+        } else if (window.console && console.log) {
+            window.console.log(s);
+        }
+        // Enhancement? log to a debug DIV inside the page.
+    }
+
+    return NDebug;
+})();

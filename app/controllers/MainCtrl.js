aa1a393105eff39573f87cd44ad3f3f763d84d12
swhp <sukma.wardana@mail.com>
2016-12-18 12:54:02 +0700

    feature generate done

--- app/controllers/MainCtrl.js
+++ app/controllers/MainCtrl.js
@@ -8,6 +8,50 @@
  * Controller for Main Page
  */
 angular.module('nanodesuApp')
-    .controller('MainCtrl', function($log, $scope){
+    .controller('MainCtrl', function($log, $scope, alertify, ApiService){
+        var preview = '/build';
+        var deploy = '/build/deploy';
+        $scope.panel = false; //in purpose to hide or shown panel deploy and preview button
+
+        /**
+         * @ngdoc method
+         * @name panelAction
+         * @methodOf nanodesuApp.controller.MainCtrl
+         * @description
+         * function to change $scope.panel value
+         *
+         * @param {boolean} true/false
+         */
+        $scope.panelAction = function(param){
+            $scope.panel = param;
+        }
+
+        $scope.preview = function(){
+            ApiService.setUrl(preview);
+            ApiService.http().query(
+                function(success){
+                    $log.debug(success);
+                    alertify.success('Success! Please Check at /test');
+                },
+                function(error){
+                    $log.debug(error);
+                    alertify.error('Error! Please Contact Admin');
+                }
+            );
+        }
+
+        $scope.deploy = function(){
+            ApiService.setUrl(deploy);
+            ApiService.http().query(
+                function(success){
+                    $log.debug(success);
+                    alertify.success('Success! Please Check at');
+                },
+                function(error){
+                    $log.debug(error);
+                    alertify.error('Error! Please Contact Admin');
+                }
+            );
+        }
     });
 })();

097d0c6b0b5eb23261dc08ff305c3b02ee09c245
swhp <sukma.wardana@mail.com>
2017-05-18 21:12:52 +0700

    initial alert message to replace alertify

---
+++ app/controllers/AlertCtrl.js
@@ -0,0 +1,54 @@
+(function() {
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nanodesuApp:controller:AlertCtrl
+ * @description
+ * # Retrieve all broadcast about alert
+ */
+angular.module('nanodesuApp')
+    .controller('AlertCtrl', function($log, $scope) {
+        
+        //remove alert
+        $scope.closeAlert = function(index) {
+            $scope.alerts.splice(index, 1);
+        };
+
+        $scope.$on('unauthorized', function() {
+            $scope.alerts = [{
+                    type: 'danger', 
+                    msg: 'Not Authorized User! Please contact Admin for further information'
+            }];
+        });
+
+        $scope.$on('created', function() {
+            $scope.alerts = [{
+                    type: 'success', 
+                    msg: 'Success Create Data! Your data has been successfuly saved'
+            }];
+        });
+        
+        $scope.$on('updated', function() {
+            $scope.alerts = [{
+                    type: 'success', 
+                    msg: 'Success Update Data! Your data has been successfuly updated'
+            }];
+        });
+
+        $scope.$on('error', function() {
+            $scope.alerts = [{
+                    type: 'danger', 
+                    msg: 'Failed! There was a problem in the server, please contact Admin'
+            }];
+        });
+        
+        $scope.$on('success', function() {
+            $scope.alerts = [{
+                    type: 'success', 
+                    msg: 'Success! Your operation is success.'
+            }];
+        });
+        
+    });
+})(); 
--- app/services/Interceptor.js
+++ app/services/Interceptor.js
@@ -9,7 +9,7 @@
  * intercept any $http request and response
  */
 angular.module('nanodesuApp')
-    .factory('ndInterceptor', function($log, $rootScope) {
+    .factory('ndInterceptor', function($q, $log, $rootScope) {
 
         var loadings = 0;
         var token = localStorage.getItem('token');
@@ -25,21 +25,35 @@ angular.module('nanodesuApp')
                 return config;
             },
             response: function(response) {
-                $log.debug('response interceptor' + response);
+                $log.debug('response interceptor');
 
                 if((--loadings) === 0) {
                     $rootScope.$broadcast('loader_hide');
+                }
+
+                if(response.status === 200) {
+                    if(response.config.method === 'POST') {
+                        $rootScope.$broadcast('created');
+                    } else if(response.config.method === 'PUT') {
+                        $rootScope.$broadcast('updated');
+                    }
                 }
 
                 return response;
             },
             responseError: function(response) {
                 // TODO: perform error message by checking the response code.
-                $log.debug('responseError interceptor' + response);
+                $log.debug('responseError interceptor');
+                if(response.status === 401) {
+                    $rootScope.$broadcast('unauthorized');
+                }
                 
-                $rootScope.$broadcast('loader_hide');
+                if((--loadings) === 0) {
+                    $rootScope.$broadcast('loader_hide');
+                }
+                $rootScope.$broadcast('error');
                 
-                return response;
+                return $q.reject(response);
             }
         };
 
